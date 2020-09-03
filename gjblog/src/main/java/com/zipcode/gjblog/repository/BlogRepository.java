aa877565091909130940106dbb3193c634f931ba
jyothi14691 <jyothi14691@gmail.com>
2020-09-03 16:14:59 -0400

    add changes with merge

--- gjblog/src/main/java/com/zipcode/gjblog/repository/BlogRepository.java
+++ gjblog/src/main/java/com/zipcode/gjblog/repository/BlogRepository.java
@@ -12,9 +12,4 @@ public interface BlogRepository extends CrudRepository<Post,Long> {
 
     List<Post> findByTag(String tag);
 
-    Post findByTag(String tag);
-
-    @Query(value = "SELECT * FROM POST p WHERE p.TAG = ?1",nativeQuery = true)
-    List<Post> findAllByTag(String tag);
-
 }
