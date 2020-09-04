306eb2ce2ee092040762e0c2768b428d37d816aa
jyothi14691 <jyothi14691@gmail.com>
2020-09-04 00:25:04 -0400

    myFirstHeroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dspring.profiles.active=postgres -jar gjblog/target/gjblog-0.0.1-SNAPSHOT.jar
---
+++ gjblog/src/main/java/com/zipcode/gjblog/config/HerokuConfigCloud.java
@@ -0,0 +1,66 @@
+package com.zipcode.gjblog.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class HerokuConfigCloud {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    @Profile("postgres")
+    public DataSource postgresDataSource() {
+        //String databaseUrl = System.getenv("DATABASE_URL");
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        //String username = dbUri.getUserInfo().split(":")[0];
+        //String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+//                + dbUri.getPort() + dbUri.getPath();
+
+        log.info("dbUri: ", dbUri.toString());
+        log.info("username :", username);
+        log.info("password :", password);
+        log.info("dbUrl :", databaseUrl);
+
+        // fully-qualified class name to distuinguish from javax.sql.DataSource
+        org.apache.tomcat.jdbc.pool.DataSource dataSource
+                = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+}
